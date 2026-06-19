1997141bfc00fc66706aabc07ff4a91c82afb95a
avans-marc <maa.mathijssen@avans.nl>
2026-06-19 19:39:45 +0200

    Tanstack Query assignment

--- code-gen/src/main.ts
+++ code-gen/src/main.ts
@@ -1,7 +1,43 @@
-// TODO: import the generated functions after running `npm run generate`
-import { getRandomFact, getTodayFact, getFactById } from "./generated/uselessfacts";
+import { QueryClient } from "@tanstack/query-core"; // Pas nodig bij opdracht 2
+import { Fact, getRandomFact } from "./generated/uselessfacts";
 
+
+
+// OPDRACHT 1 -- Toon een random fact wanneer er op de knop wordt gedrukt
 document.getElementById("btn-random")!.addEventListener("click", async () => {
-  // TODO: call getRandomFact() and display the result in #output-random
+
 
 });
+
+
+// OPDRACHT 2 -- Pas decommenten wanneer de docent dat zegt;
+
+
+
+/*
+// Bij laden: twee componenten renderen
+document.addEventListener("DOMContentLoaded", () =>
+  Promise.all([renderComponent1(), renderComponent2()])
+);
+
+
+async function renderComponent1() {
+  const fact = await fetchRandomFact(Infinity);
+  document.getElementById('component1-output')!.innerText = fact!.text;
+} 
+
+async function renderComponent2() {
+  const fact = await fetchRandomFact(Infinity);
+  document.getElementById('component2-output')!.innerText = fact!.text;
+} 
+
+
+const queryClient = new QueryClient();
+
+async function fetchRandomFact(staleTime: number) : Promise<Fact> {
+
+
+
+  return result.data;
+  }
+*/
